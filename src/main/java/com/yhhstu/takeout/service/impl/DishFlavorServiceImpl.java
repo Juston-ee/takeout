6fc49f23119a77e13eddd67b24a4bf93be915a49
Juston-ee <2567125813@qq.com>
2022-07-17 00:48:54 +0800

    完成新增菜品、菜品信息分页查询及菜品修改

---
+++ src/main/java/com/yhhstu/takeout/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,16 @@
+package com.yhhstu.takeout.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yhhstu.takeout.entity.DishFlavor;
+import com.yhhstu.takeout.mapper.DishFlavorMapper;
+import com.yhhstu.takeout.service.DishFlavorService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: YHH
+ * @version: 1.0
+ **/
+
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+}
